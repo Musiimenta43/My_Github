21c848d874712388f5073b52131ff3e2b25ec0fb
Musiimenta Maria Christine <christianamaria43@gmail.com>
2025-03-04 23:28:18 +0300

    Update mind_map.py

--- mind_map.py
+++ mind_map.py
@@ -60,7 +60,6 @@ def edit_node_label(n_clicks, node_data, new_label):
             if node["data"]["id"] == node_data["id"]:
                 node["data"]["label"] = new_label
                 break
-        return nodes + edges
     return nodes + edges
 
 if __name__ == '__main__':
